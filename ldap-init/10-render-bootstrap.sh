2b131888820f69cb35f57474d1d2b02b485923cc
UKBey <ukbetaha61@gmail.com>
2026-04-13 21:16:08 +0300

    0.9.6: Ufak bir bug fix

--- ldap-init/10-render-bootstrap.sh
+++ ldap-init/10-render-bootstrap.sh
@@ -9,7 +9,23 @@ if [ ! -f "$TEMPLATE" ]; then
   exit 1
 fi
 
-VARS='${LDAP_CUSTOMER_PASSWORD} ${LDAP_AGENT_PASSWORD} ${LDAP_MANAGER_PASSWORD}'
-envsubst "$VARS" < "$TEMPLATE" > "$OUTPUT"
+required_vars=(LDAP_CUSTOMER_PASSWORD LDAP_AGENT_PASSWORD LDAP_MANAGER_PASSWORD)
+for var_name in "${required_vars[@]}"; do
+  if [ -z "${!var_name:-}" ]; then
+    echo "[ldap-init] Missing required variable: $var_name"
+    exit 1
+  fi
+done
+
+# sed replacement icin olasi delimiter karakterlerini kacisla
+cust_pw_escaped=$(printf '%s' "$LDAP_CUSTOMER_PASSWORD" | sed 's/[&|]/\\&/g')
+agent_pw_escaped=$(printf '%s' "$LDAP_AGENT_PASSWORD" | sed 's/[&|]/\\&/g')
+manager_pw_escaped=$(printf '%s' "$LDAP_MANAGER_PASSWORD" | sed 's/[&|]/\\&/g')
+
+sed \
+  -e "s|\${LDAP_CUSTOMER_PASSWORD}|$cust_pw_escaped|g" \
+  -e "s|\${LDAP_AGENT_PASSWORD}|$agent_pw_escaped|g" \
+  -e "s|\${LDAP_MANAGER_PASSWORD}|$manager_pw_escaped|g" \
+  "$TEMPLATE" > "$OUTPUT"
 
 echo "[ldap-init] Rendered LDAP bootstrap file: $OUTPUT"
